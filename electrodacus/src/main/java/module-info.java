b2569b6e193c2b942c2010925fdad62c768ddb98
Scott Serbousek <airetamstrm@gmail.com>
2020-08-06 20:09:22 -0400

    VEDirect Baud rate

--- electrodacus/src/main/java/module-info.java
+++ electrodacus/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module io.freedriver.eletrodacus {
     requires java.logging;
     requires io.freedriver.math;
     requires io.freedriver.serial.api;
-    requires io.freedriver.serial;
+    requires io.freedriver.serial.impl;
     exports io.freedriver.electrodacus.sbms;
 }
