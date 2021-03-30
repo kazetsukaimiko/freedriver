8380686c1745a5e5922fa78b3db678658e237cf9
Scott Serbousek <airetamstrm@gmail.com>
2021-03-29 21:34:49 -0400

    Float -> Double

--- jsonlink/src/main/java/io/freedriver/jsonlink/config/v2/AlertMatching.java
+++ jsonlink/src/main/java/io/freedriver/jsonlink/config/v2/AlertMatching.java
@@ -5,23 +5,23 @@ import java.util.stream.Stream;
 public enum AlertMatching {
     ANY {
         @Override
-        public boolean test(float benchmark, AnalogAlertCondition condition, Stream<Float> actual) {
+        public boolean test(double benchmark, AnalogAlertCondition condition, Stream<Double> actual) {
             return actual.anyMatch(sample -> condition.test(benchmark, sample));
         }
     },
     ALL {
         @Override
-        public boolean test(float benchmark, AnalogAlertCondition condition, Stream<Float> actual) {
+        public boolean test(double benchmark, AnalogAlertCondition condition, Stream<Double> actual) {
             return actual.allMatch(sample -> condition.test(benchmark, sample));
         }
     },
     NONE {
         @Override
-        public boolean test(float benchmark, AnalogAlertCondition condition, Stream<Float> actual) {
+        public boolean test(double benchmark, AnalogAlertCondition condition, Stream<Double> actual) {
             return actual.noneMatch(sample -> condition.test(benchmark, sample));
         }
     };
 
 
-    public abstract boolean test(float benchmark, AnalogAlertCondition condition, Stream<Float> actual);
+    public abstract boolean test(double benchmark, AnalogAlertCondition condition, Stream<Double> actual);
 }
--- jsonlink/src/main/java/io/freedriver/jsonlink/config/v2/AnalogAlertCondition.java
+++ jsonlink/src/main/java/io/freedriver/jsonlink/config/v2/AnalogAlertCondition.java
@@ -3,16 +3,16 @@ package io.freedriver.jsonlink.config.v2;
 public enum AnalogAlertCondition {
     LESS_THAN {
         @Override
-        public boolean test(float benchmark, float actual) {
+        public boolean test(double benchmark, double actual) {
             return actual < benchmark;
         }
     }, // Percent
     MORE_THAN {
         @Override
-        public boolean test(float benchmark, float actual) {
+        public boolean test(double benchmark, double actual) {
             return actual > benchmark;
         }
     }; // Percent
 
-    public abstract boolean test(float benchmark, float actual);
+    public abstract boolean test(double benchmark, double actual);
 }
